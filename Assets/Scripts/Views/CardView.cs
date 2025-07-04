71d388b8e57a231775b4a025294f1a1ef10c5bf3
xuxuxuJS <129388808+xuxuxuJS@users.noreply.github.com>
2025-07-04 19:57:41 +0800

    导入美术资源，制作卡牌预制体

---
+++ Assets/Scripts/Views/CardView.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class CardView : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text tittle;
+
+    [SerializeField]
+    private TMP_Text description;
+
+    [SerializeField]
+    private TMP_Text mana;
+
+    [SerializeField]
+    private SpriteRenderer imgSR;
+
+    [SerializeField]
+    private GameObject wrapper;
+}
